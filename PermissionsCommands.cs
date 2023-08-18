a383a7f423cef9257e43f16e4b6f72cea474ffe5
RainOrigami <rain+github@codefreak.net>
2023-08-18 11:15:09 +0200

    Remove obsolete constructor, fix namespace

--- PermissionsCommands.cs
+++ PermissionsCommands.cs
@@ -3,38 +3,33 @@ using BBRAPIModules;
 using Commands;
 using Permissions;
 
-namespace PermissionsManager
+namespace PermissionsManager;
+
+[RequireModule(typeof(PlayerPermissions))]
+[RequireModule(typeof(CommandHandler))]
+public class PermissionsCommands : BattleBitModule
 {
-    [RequireModule(typeof(PlayerPermissions))]
-    [RequireModule(typeof(CommandHandler))]
-    public class PermissionsCommands : BattleBitModule
+    [ModuleReference]
+    public PlayerPermissions PlayerPermissions { get; set; }
+    [ModuleReference]
+    public CommandHandler CommandHandler { get; set; }
+
+    public override void OnModulesLoaded()
     {
-        [ModuleReference]
-        public PlayerPermissions PlayerPermissions { get; set; }
-        [ModuleReference]
-        public CommandHandler CommandHandler { get; set; }
+        this.CommandHandler.Register(this);
+    }
 
-        public PermissionsCommands(RunnerServer server) : base(server)
-        {
-        }
+    [CommandCallback("addperm", Description = "Adds a permission to a player", AllowedRoles = Roles.Admin)]
+    public void AddPermissionCommand(RunnerPlayer commandSource, RunnerPlayer player, Roles permission)
+    {
+        this.PlayerPermissions.AddPlayerRoles(player.SteamID, permission);
+        commandSource.Message($"Added permission {permission} to {player.Name}");
+    }
 
-        public override void OnModulesLoaded()
-        {
-            this.CommandHandler.Register(this);
-        }
-
-        [CommandCallback("addperm", Description = "Adds a permission to a player", AllowedRoles = Roles.Admin)]
-        public void AddPermissionCommand(RunnerPlayer commandSource, RunnerPlayer player, Roles permission)
-        {
-            this.PlayerPermissions.AddPlayerRoles(player.SteamID, permission);
-            commandSource.Message($"Added permission {permission} to {player.Name}");
-        }
-
-        [CommandCallback("removeperm", Description = "Removes a permission from a player", AllowedRoles = Roles.Admin)]
-        public void RemovePermissionCommand(RunnerPlayer commandSource, RunnerPlayer player, Roles permission)
-        {
-            this.PlayerPermissions.RemovePlayerRoles(player.SteamID, permission);
-            commandSource.Message($"Removed permission {permission} from {player.Name}");
-        }
+    [CommandCallback("removeperm", Description = "Removes a permission from a player", AllowedRoles = Roles.Admin)]
+    public void RemovePermissionCommand(RunnerPlayer commandSource, RunnerPlayer player, Roles permission)
+    {
+        this.PlayerPermissions.RemovePlayerRoles(player.SteamID, permission);
+        commandSource.Message($"Removed permission {permission} from {player.Name}");
     }
 }

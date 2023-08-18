754b40b9b047306761a2d2e99f289a5ece359cdb
RainOrigami <rain+github@codefreak.net>
2023-08-18 11:15:49 +0200

    Remove obsolete constructor, fix namespace

--- PlayerPermissions.cs
+++ PlayerPermissions.cs
@@ -3,83 +3,78 @@ using BBRAPIModules;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Permissions
+namespace Permissions;
+
+public class PlayerPermissions : BattleBitModule
 {
-    public class PlayerPermissions : BattleBitModule
+    public static PlayerPermissionsConfiguration Configuration { get; set; }
+
+    public override Task OnPlayerJoiningToServer(ulong steamID, PlayerJoiningArguments args)
     {
-        public static PlayerPermissionsConfiguration Configuration { get; set; }
-
-        public PlayerPermissions(RunnerServer server) : base(server)
-        {
-        }
-
-        public override Task OnPlayerJoiningToServer(ulong steamID, PlayerJoiningArguments args)
-        {
-            args.Stats.Roles = this.GetPlayerRoles(steamID);
-            return Task.CompletedTask;
-        }
-
-        public override Task OnPlayerConnected(RunnerPlayer player)
-        {
-            lock (Configuration.PlayerRoles)
-            {
-                if (!Configuration.PlayerRoles.ContainsKey(player.SteamID))
-                {
-                    Configuration.PlayerRoles.Add(player.SteamID, Roles.None);
-                }
-            }
-
-            return Task.CompletedTask;
-        }
-
-        public bool HasPlayerRole(ulong steamID, Roles role)
-        {
-            return (this.GetPlayerRoles(steamID) & role) == role;
-        }
-
-        public Roles GetPlayerRoles(ulong steamID)
-        {
-            lock (Configuration.PlayerRoles)
-            {
-                if (Configuration.PlayerRoles.ContainsKey(steamID))
-                {
-                    return Configuration.PlayerRoles[steamID];
-                }
-            }
-
-            return Roles.None;
-        }
-
-        public void SetPlayerRoles(ulong steamID, Roles roles)
-        {
-            lock (Configuration.PlayerRoles)
-            {
-                if (Configuration.PlayerRoles.ContainsKey(steamID))
-                {
-                    Configuration.PlayerRoles[steamID] = roles;
-                }
-                else
-                {
-                    Configuration.PlayerRoles.Add(steamID, roles);
-                }
-            }
-
-            Configuration.Save();
-        }
-
-        public void AddPlayerRoles(ulong steamID, Roles role)
-        {
-            this.SetPlayerRoles(steamID, this.GetPlayerRoles(steamID) | role);
-        }
-
-        public void RemovePlayerRoles(ulong steamID, Roles role)
-        {
-            this.SetPlayerRoles(steamID, this.GetPlayerRoles(steamID) & ~role);
-        }
+        args.Stats.Roles = this.GetPlayerRoles(steamID);
+        return Task.CompletedTask;
     }
 
-    public class PlayerPermissionsConfiguration : ModuleConfiguration
+    public override Task OnPlayerConnected(RunnerPlayer player)
     {
-        public Dictionary<ulong, Roles> PlayerRoles { get; set; } = new();
+        lock (Configuration.PlayerRoles)
+        {
+            if (!Configuration.PlayerRoles.ContainsKey(player.SteamID))
+            {
+                Configuration.PlayerRoles.Add(player.SteamID, Roles.None);
+            }
+        }
+
+        return Task.CompletedTask;
     }
+
+    public bool HasPlayerRole(ulong steamID, Roles role)
+    {
+        return (this.GetPlayerRoles(steamID) & role) == role;
+    }
+
+    public Roles GetPlayerRoles(ulong steamID)
+    {
+        lock (Configuration.PlayerRoles)
+        {
+            if (Configuration.PlayerRoles.ContainsKey(steamID))
+            {
+                return Configuration.PlayerRoles[steamID];
+            }
+        }
+
+        return Roles.None;
+    }
+
+    public void SetPlayerRoles(ulong steamID, Roles roles)
+    {
+        lock (Configuration.PlayerRoles)
+        {
+            if (Configuration.PlayerRoles.ContainsKey(steamID))
+            {
+                Configuration.PlayerRoles[steamID] = roles;
+            }
+            else
+            {
+                Configuration.PlayerRoles.Add(steamID, roles);
+            }
+        }
+
+        Configuration.Save();
+    }
+
+    public void AddPlayerRoles(ulong steamID, Roles role)
+    {
+        this.SetPlayerRoles(steamID, this.GetPlayerRoles(steamID) | role);
+    }
+
+    public void RemovePlayerRoles(ulong steamID, Roles role)
+    {
+        this.SetPlayerRoles(steamID, this.GetPlayerRoles(steamID) & ~role);
+    }
+}
+
+public class PlayerPermissionsConfiguration : ModuleConfiguration
+{
+    public Dictionary<ulong, Roles> PlayerRoles { get; set; } = new();
 }

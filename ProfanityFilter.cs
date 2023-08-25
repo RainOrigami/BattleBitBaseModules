12cae4e19cf7ece2a6586eae6e415c2e3f2bc698
RainOrigami <rain+github@codefreak.net>
2023-08-25 17:20:45 +0200

    Adds simple profanity filter base module

---
+++ ProfanityFilter.cs
@@ -0,0 +1,30 @@
+﻿using BattleBitAPI.Common;
+using BBRAPIModules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleBitBaseModules;
+
+public class ProfanityFilter : BattleBitModule
+{
+    public ProfanityFilterConfiguration Configuration { get; set; }
+
+    public override Task<bool> OnPlayerTypedMessage(RunnerPlayer player, ChatChannel channel, string message)
+    {
+        if (this.Configuration.Profanity.Any(x => message.Contains(x, StringComparison.OrdinalIgnoreCase)))
+        {
+            player.Message("Please do not use profanity in chat.", this.Configuration.MessageTimeout);
+            return Task.FromResult(false);
+        }
+
+        return Task.FromResult(true);
+    }
+}
+
+public class ProfanityFilterConfiguration : BattleBitModule
+{
+    public List<string> Profanity { get; set; } = new();
+    public float MessageTimeout { get; set; } = 10f;
+}

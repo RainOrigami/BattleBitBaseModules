6cc37e36f9559a59a1618d28e486ed6f6b2cc9d3
RainOrigami <rain+github@codefreak.net>
2023-08-25 23:43:37 +0200

    Adds SpectateControl base module

---
+++ SpectateControl.cs
@@ -0,0 +1,26 @@
+﻿using BattleBitAPI.Common;
+using BBRAPIModules;
+using Permissions;
+using System.Threading.Tasks;
+
+namespace BattleBitBaseModules;
+
+[RequireModule(typeof(PlayerPermissions))]
+public class SpectateControl : BattleBitModule
+{
+    public static SpectateControlConfiguration Configuration { get; set; }
+
+    public PlayerPermissions PlayerPermissions { get; set; }
+
+    public override Task OnPlayerConnected(RunnerPlayer player)
+    {
+        player.Modifications.CanSpectate = (PlayerPermissions.GetPlayerRoles(player.SteamID) & (Roles)Configuration.SpectatorRoles) > 0;
+
+        return Task.CompletedTask;
+    }
+}
+
+public class SpectateControlConfiguration : ModuleConfiguration
+{
+    public ulong SpectatorRoles { get; set; } = (ulong)(Roles.Admin | Roles.Moderator);
+}

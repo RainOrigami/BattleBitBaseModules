60166187569aa5647dbe9890025489852f9ff00c
RainOrigami <rain+github@codefreak.net>
2023-09-02 10:53:22 +0200

    basic voting

---
+++ Voting.cs
@@ -0,0 +1,152 @@
+﻿using BattleBitAPI.Common;
+using BBRAPIModules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleBitBaseModules;
+
+/// <summary>
+/// Author: @RainOrigami
+/// Version: 0.4.11
+/// </summary>
+/// 
+
+public class Voting : BattleBitModule
+{
+    private bool activeVote = false;
+    private string voteText = "";
+    private string[] voteOptions = new string[0];
+    private Dictionary<ulong, int> votes = new();
+    private DateTime endOfVote = DateTime.MinValue;
+
+    public VoteConfiguration Configuration { get; set; }
+
+    [ModuleReference]
+    public dynamic? RichText { get; set; }
+
+    public void StartVoteCommand(RunnerPlayer commandSource, string text, string options)
+    {
+        if (this.activeVote)
+        {
+            commandSource.Message("There is already an active vote.");
+            return;
+        }
+
+        this.activeVote = true;
+        this.voteText = text;
+        this.voteOptions = options.Split('|');
+
+        if (this.voteOptions.Length >= 10)
+        {
+            commandSource.Message("You can only have up to 9 options.");
+            this.activeVote = false;
+            return;
+        }
+
+        this.votes.Clear();
+        this.endOfVote = DateTime.Now.AddSeconds(this.Configuration.VoteDuration);
+
+        this.Server.SayToAllChat($"{this.RichText?.Size(125)}A vote has been started!");
+        this.Server.SayToAllChat($"Vote: {this.voteText}");
+
+        StringBuilder messageText = new($"{this.RichText?.Size(125)}{this.voteText}{this.RichText?.Size(100)}{Environment.NewLine}");
+        for (int i = 0; i < this.voteOptions.Length; i++)
+        {
+            messageText.AppendLine($"Type {i + 1} in chat for {this.RichText?.FromColorName("yellow")}{this.voteOptions[i]}");
+        }
+
+        messageText.AppendLine($"{this.RichText?.Size(125)}{Environment.NewLine}You have {this.Configuration.VoteDuration} seconds to vote.");
+
+        foreach (RunnerPlayer player in this.Server.AllPlayers)
+        {
+            player.Message($"{this.RichText?.Size(125)}{this.voteText}{Environment.NewLine}");
+        }
+
+        this.Server.SayToAllChat(messageText.ToString());
+
+        Task.Run(voteHandler);
+    }
+
+    private void voteHandler()
+    {
+        while (this.IsLoaded && this.Server.IsConnected && this.activeVote)
+        {
+            if (DateTime.Now > this.endOfVote)
+            {
+                this.activeVote = false;
+                break;
+            }
+
+            Task.Delay(1000).Wait();
+        }
+
+        if (this.votes.Count == 0)
+        {
+            this.Server.SayToAllChat($"{this.RichText?.Size(125)}The vote has ended!{Environment.NewLine}Nobody voted.");
+            return;
+        }
+
+        int[] voteCounts = new int[this.voteOptions.Length];
+        foreach (int vote in this.votes.Values)
+        {
+            voteCounts[vote - 1]++;
+        }
+
+        int maxVotes = voteCounts.Max();
+        int maxVoteIndex = voteCounts.ToList().IndexOf(maxVotes);
+
+        this.Server.SayToAllChat($"{this.RichText?.Size(125)}The vote has ended!{Environment.NewLine}{this.RichText?.FromColorName("yellow")}{this.voteOptions[maxVoteIndex]}{this.RichText?.Color()} won with {maxVotes} votes.");
+    }
+
+    public override async Task<bool> OnPlayerTypedMessage(RunnerPlayer player, ChatChannel channel, string msg)
+    {
+        if (!this.activeVote)
+        {
+            return true;
+        }
+
+        msg = new string(msg.Where(c => char.IsDigit(c)).Distinct().ToArray());
+        if (msg.Length == 0)
+        {
+            return true;
+        }
+
+        if (msg.Length > 0)
+        {
+            player.SayToChat("Could not find a unique vote option in your message.");
+            return true;
+        }
+
+        if (!int.TryParse(msg, out int vote))
+        {
+            return true;
+        }
+
+        if (vote < 1 || vote > this.voteOptions.Length)
+        {
+            return true;
+        }
+
+        if (this.votes.ContainsKey(player.SteamID))
+        {
+            this.votes[player.SteamID] = vote;
+            return true;
+        }
+
+        this.votes.Add(player.SteamID, vote);
+
+        player.Message($"You voted for {this.RichText?.FromColorName("yellow")}{this.voteOptions[vote - 1]}{this.RichText?.Color()}. You can change your vote any time.");
+
+        await Task.CompletedTask;
+
+        return true;
+    }
+}
+
+public class VoteConfiguration : ModuleConfiguration
+{
+    public int VoteDuration { get; set; } = 60;
+}

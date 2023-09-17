548aff35642bc0b4242453461c50096e55d07eb1
Bluscream <Bluscream@users.noreply.github.com>
2023-09-17 17:40:23 +0200

    Update PermissionsCommands.cs
    + Add !listperms <player>
    + Add !clearperms <player>

--- PermissionsCommands.cs
+++ PermissionsCommands.cs
@@ -1,13 +1,17 @@
-﻿using BattleBitAPI.Common;
+using BattleBitAPI.Common;
 using BBRAPIModules;
 using Commands;
 using Permissions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace PermissionsManager;
 
 /// <summary>
 /// Author: @RainOrigami
-/// Version: 0.4.5.1
+/// Version: 0.4.5.2
 /// </summary>
 
 [RequireModule(typeof(PlayerPermissions))]
@@ -19,10 +23,7 @@ public class PermissionsCommands : BattleBitModule
     [ModuleReference]
     public CommandHandler CommandHandler { get; set; }
 
-    public override void OnModulesLoaded()
-    {
-        this.CommandHandler.Register(this);
-    }
+    public override void OnModulesLoaded() => this.CommandHandler.Register(this);
 
     [CommandCallback("addperm", Description = "Adds a permission to a player", AllowedRoles = Roles.Admin)]
     public void AddPermissionCommand(RunnerPlayer commandSource, RunnerPlayer player, Roles permission)
@@ -37,4 +38,33 @@ public class PermissionsCommands : BattleBitModule
         this.PlayerPermissions.RemovePlayerRoles(player.SteamID, permission);
         commandSource.Message($"Removed permission {permission} from {player.Name}");
     }
+
+    [CommandCallback("clearperms", Description = "Removes all permission from a player", AllowedRoles = Roles.Admin)]
+    public void ClearPermissionCommand(RunnerPlayer commandSource, RunnerPlayer player) => RemovePermissionCommand(commandSource, player, (Roles)15);
+
+    [CommandCallback("listperms", Description = "Lists player permissions", AllowedRoles = Roles.Admin | Roles.Moderator)]
+    public void ListPermissionCommand(RunnerPlayer commandSource, RunnerPlayer? targetPlayer = null) {
+        var sb = new StringBuilder();
+        if (targetPlayer is null) {
+            foreach (var permissionsPlayer in PlayerPermissions.Configuration.PlayerRoles) {
+                var roles = GetIndividualRoleStrings(permissionsPlayer.Value);
+                sb.AppendLine($"{GetPlayerNameOrId(permissionsPlayer.Key)}: {string.Join(", ", roles)}");
+            }
+        } else {
+            var roles = GetIndividualRoleStrings(PlayerPermissions.GetPlayerRoles(targetPlayer.SteamID));
+            sb.AppendLine($"{GetPlayerNameOrId(targetPlayer.SteamID)}: {string.Join('\n', roles)}");
+        }
+        commandSource.Message(sb.ToString());
+    }
+
+    public Roles[] GetIndividualRoles(Roles roles) {
+        List<Roles> permStrs = new();
+        foreach (var role in Enum.GetValues<Roles>()) {
+            if ((role & roles) > 0) permStrs.Add(role);
+        }
+        return permStrs.ToArray();
+    }
+    public string[] GetIndividualRoleStrings(Roles roles) => GetIndividualRoles(roles).Select(r => r.ToString()).ToArray();
+    public string GetPlayerNameOrId(ulong steamId64) => this.Server.AllPlayers.FirstOrDefault(p => p.SteamID == steamId64)?.Name ?? steamId64.ToString();
+
 }

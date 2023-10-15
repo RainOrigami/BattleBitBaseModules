0bddfe2e943880ca21d9a3efa8fdc61948dfcdb7
Bluscream <Bluscream@users.noreply.github.com>
2023-10-15 03:11:19 +0200

    add maptime + use richtext

--- LoadingScreenText.cs
+++ LoadingScreenText.cs
@@ -1,6 +1,8 @@
 ﻿using BattleBitAPI.Common;
 using BBRAPIModules;
 using Bluscream;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,7 +14,7 @@ namespace BattleBitBaseModules;
 public class LoadingScreenText : BattleBitModule
 {
     [ModuleReference]
-    public LoadingScreenTextConfiguration Configuration { get; set; } = null!;
+    public LoadingScreenTextConfiguration Config { get; set; } = null!;
 
     public override Task OnConnected() {
         UpdateLoadingScreenText(); return Task.CompletedTask;
@@ -22,16 +24,33 @@ public class LoadingScreenText : BattleBitModule
         UpdateLoadingScreenText(); return Task.CompletedTask;
     }
 
+    public override Task OnGameStateChanged(GameState oldState, GameState newState) {
+        UpdateLoadingScreenText(); return Task.CompletedTask;
+    }
+
     public void UpdateLoadingScreenText() {
-        this.Server.LoadingScreenText = this.Configuration.LoadingScreenText
+        var newText = this.Config.LoadingScreenText
             .Replace("{server}", this.Server.ServerName)
-            .Replace("{gamemode}", this.Server.Gamemode.ToTitleCase())
-            .Replace("{map}", this.Server.Map.ToTitleCase())
+            .Replace("{gamemode}", this.Server.GetCurrentGameMode().DisplayName)
+            .Replace("{map}", this.Server.GetCurrentMap().DisplayName)
+            .Replace("{maptime}", this.Server.DayNight.ToString().ToTitleCase())
             .Replace("{players}", this.Server.AllPlayers.Count().ToString())
             .Replace("{slots}", this.Server.MaxPlayerCount.ToString());
+
+        foreach (var replacement in Config.randomReplacements) {
+            newText = newText.Replace($"{{random.{replacement.Key}}}", replacement.Value[Random.Shared.Next(replacement.Value.Length)]);
+        }
+
+        this.Server.LoadingScreenText = newText;
     }
 
     public class LoadingScreenTextConfiguration : ModuleConfiguration {
-        public string LoadingScreenText { get; set; } = "<#87CEEB>Welcome to <color=\"white\">{server}<#87CEEB>!<br>We are currently playing <color=\"white\">{gamemode}<#87CEEB> on <color=\"white\">{map}<#87CEEB> with <color=\"white\">{players}<#87CEEB>/<color=\"white\">{slots}<#87CEEB> players!<br>Enjoy your stay!";
+        public Dictionary<string, string[]> randomReplacements = new Dictionary<string, string[]>() {
+            { "welcome", new string[] { "Enjoy your stay!", "Have a good one!", "Get Ready for battle!" } },
+        };
+        public string LoadingScreenText { get; set; } =
+@$"{Colors.SkyBlue}Welcome to {Colors.None}{{server}}{Colors.SkyBlue}!
+We are currently playing {Colors.None}{{gamemode}}{Colors.SkyBlue} on {Colors.None}{{map}}{Colors.SkyBlue} at {Colors.None}{{maptime}}{Colors.SkyBlue} with {Colors.None}{{players}}{Colors.SkyBlue}/{Colors.None}{{slots}}{Colors.SkyBlue} players!
+{{random.welcome}}";
     }
 }

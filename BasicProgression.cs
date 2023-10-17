1643e9531d71145cec056801e000f7696cf3bf28
Bluscream <Bluscream@users.noreply.github.com>
2023-10-17 07:21:24 +0200

    make BasicProgression gr8 again

--- BasicProgression.cs
+++ BasicProgression.cs
@@ -2,91 +2,109 @@
 using BBRAPIModules;
 using System;
 using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Bluscream;
+using System.Text.Json.Serialization;
+using System.ComponentModel.Design;
 
-namespace BattleBitBaseModules;
+namespace BattleBitBaseModules {
 
-[Module("Provide basic persistent progression for players", "1.1.0")]
-public class BasicProgression : BattleBitModule
-{
-    public BasicProgressionConfiguration Configuration { get; set; } = null!;
+    [RequireModule(typeof(BluscreamLib))]
+    [Module("Provide basic persistent progression for players", "1.1.0")]
+    public class BasicProgression : BattleBitModule {
+        [ModuleReference]
+        public BluscreamLib BluscreamLib { get; set; } = null!;
 
-    private string dataDir => this.Configuration.PerServer ? Path.Combine(this.Configuration.DataDirectory, $"{this.Server.GameIP}:{this.Server.GamePort}") : this.Configuration.DataDirectory;
+        public Configuration GlobalConfiguration { get; set; } = null!;
 
-    public override Task OnConnected()
-    {
-        if (!Directory.Exists(dataDir))
-        {
-            Directory.CreateDirectory(dataDir);
+        public DirectoryInfo DataDir => new DirectoryInfo(GlobalConfiguration.PerServer ? Path.Combine(GlobalConfiguration.DataDirectory, $"{this.Server.GameIP}:{this.Server.GamePort}") : GlobalConfiguration.DataDirectory);
+
+        public override Task OnConnected() {
+            if (!DataDir.Exists) {
+                DataDir.Create();
+            }
+
+            return Task.CompletedTask;
         }
 
-        return Task.CompletedTask;
-    }
-
-    public override async Task OnPlayerJoiningToServer(ulong steamID, PlayerJoiningArguments args)
-    {
-        if (this.Configuration.ApplyInitialStatsOnEveryJoin)
-        {
-            args.Stats = this.Configuration.InitialStats ?? args.Stats;
-            return;
-        }
-
-        string playerFileName = getPlayerFileName(steamID);
-        for (int i = 0; i < 5; i++)
-        {
-            try
-            {
-                args.Stats = File.Exists(playerFileName) ? new PlayerStats(File.ReadAllBytes(playerFileName)) : (this.Configuration.InitialStats ?? args.Stats);
+        public override async Task OnPlayerJoiningToServer(ulong steamID, PlayerJoiningArguments args) {
+            Console.WriteLine("OnPlayerJoiningToServer");
+            if (GlobalConfiguration.ApplyInitialStatsOnEveryJoin) {
+                this.Logger.Info(args.Stats.str());
+                this.Logger.Info(args.Stats.ToJson());
+                args.Stats = GlobalConfiguration.InitialStats ?? args.Stats;
+                this.Logger.Info($"Applied initial player stats for {steamID}");
+                this.Logger.Info(args.Stats.str());
+                this.Logger.Info(args.Stats.ToJson());
+                args.Stats.Progress.Rank = 201;
+                this.Logger.Info(args.Stats.str());
+                this.Logger.Info(args.Stats.ToJson());
                 return;
             }
-            catch (Exception ex)
-            {
-                this.Logger.Error($"Tried {i} times to read from file {playerFileName} but failed:{ex}");
+
+            var playerFile = getPlayerFile(steamID);
+            for (int i = 0; i < 5; i++) {
+                try {
+                    if (playerFile.Exists) {
+                        args.Stats = GlobalConfiguration.PlainText ? JsonUtils.FromJsonFile<PlayerStats>(playerFile) : new PlayerStats(File.ReadAllBytes(playerFile.FullName));
+                        this.Logger.Info($"Read player stats for {steamID} from \"{playerFile.FullName}\"");
+                    } else {
+                        args.Stats = (GlobalConfiguration.InitialStats ?? args.Stats);
+                        this.Logger.Info($"Applied initial player stats for {steamID}");
+                    }
+                    return;
+                } catch (Exception ex) {
+                    this.Logger.Error($"Tried {i} times to read from file {playerFile.FullName} but failed:{ex}");
+                }
+                await Task.Delay(250);
             }
-            await Task.Delay(250);
+            this.Logger.Error($"Giving up trying to read \"{playerFile.FullName}\"");
         }
-        this.Logger.Error("Giving up trying to read.");
-    }
 
-    public override async Task OnSavePlayerStats(ulong steamID, PlayerStats stats)
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            try
-            {
-                File.WriteAllBytes(getPlayerFileName(steamID), stats.SerializeToByteArray());
-                return;
+        public override async Task OnSavePlayerStats(ulong steamID, PlayerStats stats) {
+            Console.WriteLine("OnSavePlayerStats");
+            this.Logger.Info(stats.str());
+            this.Logger.Info(stats.ToJson());
+            for (int i = 0; i < 5; i++) {
+                try {
+                    var playerFile = getPlayerFile(steamID);
+                    if (GlobalConfiguration.PlainText) {
+                        stats.ToJsonFile(playerFile, indented: true);
+                    } else {
+                        playerFile.WriteAllBytes(stats.SerializeToByteArray());
+                    }
+                    this.Logger.Info($"Wrote player stats for {steamID} to \"{playerFile}\"");
+                    return;
+                } catch (Exception ex) {
+                    this.Logger.Error($"Tried {i} times to write to file {getPlayerFile(steamID)} but failed:{ex}");
+                }
+                await Task.Delay(250);
             }
-            catch (Exception ex)
-            {
-                this.Logger.Error($"Tried {i} times to write to file {getPlayerFileName(steamID)} but failed:{ex}");
-            }
-            await Task.Delay(250);
+            this.Logger.Error("Giving up trying to save.");
         }
-        this.Logger.Error("Giving up trying to save.");
+
+        private FileInfo getPlayerFile(ulong steamId) {
+            return new FileInfo(Path.Combine(DataDir.FullName, $"{steamId}.bin"));
+        }
+
+        public class Configuration : ModuleConfiguration {
+            public string DataDirectory { get; set; } = "./data/PersistentProgressionFiles";
+
+            public bool PlainText { get; set; } = false;
+
+            public bool PerServer { get; set; } = false;
+
+            public bool ApplyInitialStatsOnEveryJoin { get; set; } = false;
+
+            public PlayerStats InitialStats = new PlayerStats() {
+                Achievements = new byte[0],
+                IsBanned = false,
+                Progress = new PlayerStats.PlayerProgess(),
+                Roles = Roles.None,
+                Selections = new byte[0],
+                ToolProgress = new byte[0]
+            };
+        }
     }
-
-    private string getPlayerFileName(ulong steamId)
-    {
-        return Path.Combine(dataDir, $"{steamId}.bin");
-    }
-}
-
-public class BasicProgressionConfiguration : ModuleConfiguration
-{
-    public string DataDirectory { get; set; } = "./data/PersistentProgressionFiles";
-
-    public bool PerServer { get; set; } = false;
-
-    public bool ApplyInitialStatsOnEveryJoin { get; set; } = false;
-
-    public PlayerStats? InitialStats { get; set; } = new PlayerStats()
-    {
-        Achievements = new byte[0],
-        IsBanned = false,
-        Progress = new PlayerStats.PlayerProgess(),
-        Roles = Roles.None,
-        Selections = new byte[0],
-        ToolProgress = new byte[0]
-    };
 }
